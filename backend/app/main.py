fa6c0d394707fcf6d94fab1c9791994fbbf74be6
Parth Kumar <parthkumar0607@gmail.com>
2026-07-26 11:49:32 +0530

    Add events backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,11 @@
 from app.routers.auth import router as auth_router
-from app.routers.team import router as teams_router
+from app.routers.teams import router as teams_router
 from app.routers.events import router as events_router
 from fastapi import FastAPI
 from app.models.team import Team
 from app.database.database import Base, engine
 from app.models.user import User
-from app.routers.auth import router as auth_router
+
 from app.routers.users import router as users_router
 from app.models.team_member import TeamMember
 from app.models.event import Event
