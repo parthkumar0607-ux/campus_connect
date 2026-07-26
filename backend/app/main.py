d8fc4871de4ee79744d5222c96ce4408c1cbe443
Parth Kumar <parthkumar0607@gmail.com>
2026-07-26 13:42:52 +0530

    Allow all localhost ports for Flutter web

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,14 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:59176",
-        "http://localhost:3000",
-        "http://localhost:5000",
-        "http://127.0.0.1:59176",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:5000",
-    ],
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
