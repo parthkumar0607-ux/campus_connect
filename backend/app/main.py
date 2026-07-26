f1db49f774a7522f688b1e2e42ced7a23ca59b7a
Parth Kumar <parthkumar0607@gmail.com>
2026-07-26 12:07:21 +0530

    Create event database tables

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,16 +2,17 @@ from app.routers.auth import router as auth_router
 from app.routers.teams import router as teams_router
 from app.routers.events import router as events_router
 from fastapi import FastAPI
+from app.models.user import User
 from app.models.team import Team
 from app.database.database import Base, engine
-from app.models.user import User
+
 
 from app.routers.users import router as users_router
 from app.models.team_member import TeamMember
 from app.models.event import Event
 from app.models.event_attendee import EventAttendee
 
-#Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="CampusConnect API",
